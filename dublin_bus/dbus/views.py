f9ec754e00d3e025d5bb9689db14cb004084ce71
Hugh <hugh.obrien@ucdconnect.ie>
2018-06-20 12:03:33 +0100

    getting ready for django deployment

--- dublin_bus/dbus/views.py
+++ dublin_bus/dbus/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def home(request):
 	template = loader.get_template('dbus/index.html')
 	return HttpResponse(template)
+
+class Home(TemplateView):
+	template_name = 'index.html'
