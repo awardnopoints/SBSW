3556b18eb2bfaafe5af6dd71f2227c5be5cf622a
Hugh <hugh.obrien@ucdconnect.ie>
2018-07-04 15:44:20 +0100

    added lables to forms.py

--- dublin_bus/dbus/forms.py
+++ dublin_bus/dbus/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class Predictions(forms.Form):
 	
-	start = forms.CharField()
-	end = forms.Charfield()
+	start = forms.CharField(label = "Start Stop")
+	end = forms.Charfield(label = "End Stop")
