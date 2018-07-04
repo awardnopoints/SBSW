f4260731438f2f59110865520cb8a2a9934ba201
Hugh <hugh.obrien@ucdconnect.ie>
2018-07-04 11:29:42 +0100

    recent updates

--- dublin_bus/dbus/models.py
+++ dublin_bus/dbus/models.py
@@ -1,23 +1,5 @@
 from django.db import models
 
-class Stops (models.Model):
-
-    stop_id=models.AutoField(max_length=200, primary_key=True)
-    stop_name=models.TextField()
-    latitude=models.FloatField()
-    longitude=models.FloatField()
-    stop_id_long=models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.stop_name
-
-    def get_lat(self, id):
-        return self.latitude[stop_id]
-
-    def get_long(self):
-        return self.longitude[stop_id]
-
-
 class Stopsv2 (models.Model):
 
     stop_id=models.AutoField(max_length=200, primary_key=True)
@@ -33,4 +15,15 @@ class Stopsv2 (models.Model):
         return self.latitude
 
     def get_long(self):
-        return self.longitude
+        return self.longitude	 
+
+class Trip_avg(models.Model):
+	
+	index_id=models.IntegerField(primary_key=True)
+	start_stop=models.TextField()
+	end_stop=models.TextField()
+	hour=models.FloatField()
+	minute=models.FloatField()
+	day_of_week=models.FloatField()
+	avg_time_taken=models.FloatField()
+	avg_hang=models.FloatField()
--- dublin_bus/dbus/views.py
+++ dublin_bus/dbus/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext, loader
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from dbus.models import Stopsv2
+from dbus.models import Trip_avg
 import json
 from django.utils.safestring import SafeString
 
@@ -25,3 +26,38 @@ def home(request):
 	#args['contents'] = response.json()
 
 	return render(request, 'dbus/index.html', {"contents" : stop_all})
+
+
+
+def prediction(start, end, route, hour, day, minute):
+
+	if minute >= 30:
+		minute = 45
+	else:
+		minute = 15	
+
+	fh = open("/home/student/files/routes/%s.txt"  % route)
+
+	start_stop = False
+
+	end_stretch = 0
+
+	for line in fh:
+
+		if line == start:
+
+			start_stop = True
+			end_stretch = line
+
+		elif start_stop == True and line != end:
+			
+			att = Trip_avg.objects.all().filter(hour=hour, minute=minute, day_of_week=day)
+			
+			
+			total += (int(att.avg_time_taken) + int(att.avg_hang))
+
+		elif line == end:
+
+			break
+
+	return total	
