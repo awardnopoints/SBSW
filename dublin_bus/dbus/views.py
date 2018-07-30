3030459eb7a13266c8d5385685ece31e48d078b7
eoin <awardnopoints@gmail.com>
2018-07-30 21:30:12 +0100

    slight modifications to views

--- dublin_bus/dbus/views.py
+++ dublin_bus/dbus/views.py
@@ -22,14 +22,20 @@ import datetime
 import math
 
 
-routes_implemented = ('31','130','140','14','15','16','31','39A','46A','1', '102', '104', '11', '111', '114', '116', '118', '120', '122', '123', '13', '142', '145', '150', '151', '15A', '15B', '161', '17', '17A', '18', '184', '185', '220', '236', '238', '239', '25', '25A', '25B', '25D', '25X', '26', '27', '270', '27A', '27B', '27X', '29A', '31A', '31B', '31D', '32', '32X', '33', '33A', '33B', '33X', '37', '38', '38A', '38B', '39', '4', '40', '40B', '40D', '41', '41B', '41C', '41X', '42', '42D', '43', '44', '44B', '45A', '46E', '47', '49', '51D', '51X', '53', '54A', '56A', '59', '61', '63', '65', '65B', '66', '66A', '66B', '66X', '67', '67X', '68', '68A', '68X', '69', '69X', '7', '70', '70D', '75', '757', '76', '76A', '77A', '77X', '79', '79A', '7A', '7B', '7D', '83', '84', '84A', '84X', '9')
+routes_implemented = ['31','130','140','14','15','16','31','39A','46A','1', '102', '104', '11', '111', '114', '116', '118', '120', '13', '142', '145', '150', '151', '15A', '15B', '161', '17', '17A', '18', '184', '185', '220', '236', '238', '239', '25', '25A', '25B', '25D', '25X', '26', '27', '270', '27A', '27B', '27X', '29A', '31A', '31B', '31D', '32', '32X', '33', '33A', '33B', '33X', '37', '38', '38A', '38B', '39', '4', '40', '40B', '40D', '41', '41B', '41C', '41X', '42', '42D', '43', '44', '44B', '45A', '46E', '47', '49', '51D', '51X', '53', '54A', '56A', '59', '61', '63', '65', '65B', '66', '66A', '66B', '66X', '67', '67X', '68', '68A', '68X', '69', '69X', '7', '70', '70D', '75', '757', '76', '76A', '77A', '77X', '79', '79A', '7A', '7B', '7D', '83', '84', '84A', '84X', '9']
 
-routes_to_be_implemented = ()
+routes_to_be_implemented = ('123', '122')
 
 routes_no_longer_in_service = ('83A','16C','41A','14C','38D')
 
 routes_in_service_uncovered = ('7N', '15D', '15N', '25N', '29N', '31N', '33D', '33N', '39X', '39N', '41N', '42N', '46N', '49N', '66N')
 
+routes_unsupported_by_data = ('116','118','236','25D','25X','27X','31D','32X','41X','42D','44B','46E','51D','51X','68A','68X','69X','70D','76A','77X','7D')
+
+for route in routes_unsupported_by_data:
+        routes_implemented.remove(route)
+
+print('building categories')
 stop_cats = sllz.objects.values_list('stop_id', flat=True).distinct()
 day_cats = [i for i in range(7)]
 weather_cats = ['Clouds','Rain','Drizzle','Fog','Clear','Mist','Smoke','Snow','Thunderstorm']
@@ -37,27 +43,22 @@ zone_cats = sllz.objects.values_list('zone', flat=True).distinct()
 
 
 
-def unzip_models():
-	for route in routes_implemented:
-        	for aspect in ('hangtime','traveltime'):
-                	if os.path.exists('dbus/predictive_models/{}_{}_model'.format(route, aspect)):
-                        	print('Model {}, {} found'.format(route, aspect))
-                	else:
-                        	print('Model {}, {} not found'.format(route, aspect))
-                        	if os.path.exists('dbus/predictive_models/{}_{}_model.zip'.format(route, aspect)) :
-                                	print('Zip found, unzipping.')
-                                	zip_ref = zipfile.ZipFile('dbus/predictive_models/{}_{}_rfr.zip'.format(route, aspect))
-                                	zip_ref.extractall('dbus/predictive_models')
-                                	zip_ref.close()
-                                	print('Unzipped')
-                        	else:
-                                	sys.exit('Error: No model exists for: {}, {}'.format(route, aspect))
+def find_models():
+        for route in routes_implemented:
+                for aspect in ('hangtime','traveltime'):
+                        if os.path.exists('dbus/predictive_models/{}_{}_model'.format(route, aspect)):
+                                pass
+                                #print('Model {}, {} found'.format(route, aspect))
+                        else:
+                                print('Model {}, {} not found'.format(route, aspect))
+                                sys.exit(1,'Error: No model exists for: {}, {}'.format(route, aspect))
 
 def load_models():
         models = {}
         for route in routes_implemented:
-        	models[route + '_h'] = joblib.load('dbus/predictive_models/{}_hangtime_model'.format(route))
-        	models[route + '_t'] = joblib.load('dbus/predictive_models/{}_traveltime_model'.format(route))
+                print('loading',route,'models')
+                models[route + '_h'] = joblib.load('dbus/predictive_models/{}_hangtime_model'.format(route))
+                models[route + '_t'] = joblib.load('dbus/predictive_models/{}_traveltime_model'.format(route))
         return models
 
 def stop_and_routes_info():
@@ -94,8 +95,8 @@ def stop_and_routes_info():
 
         return(mystops, myroutes)
 
-
-unzip_models()
+print('finding models')
+find_models()
 models = load_models()
 #stops, routes = stop_and_routes_info()
 
@@ -178,12 +179,12 @@ def predictions_model(start, end, route, year, month, day, hour):
         weather = forecast.objects.filter(datetime__date=datetime.date(year, month, day),
                                         datetime__hour__lte=hour+1.5
                                         )
-        print(len(weather))
+        #print(len(weather))
         if len(weather) == 0:
                 weather = forecast.objects.all().first()
         else:
                 weather = weather.last()
-        print(weather)
+        #print(weather)
         df = pd.DataFrame(input_list, columns=['stop_id','distance','zone'])
         df['day'] = date.weekday()
         df['weather_main'] = weather.mainDescription
@@ -219,7 +220,7 @@ def predictions_model(start, end, route, year, month, day, hour):
 def inputValidator(start_stop, end_stop):
         # Checks if the inputs are valid, otherwise returns False        
         if len(start_stop) == 0 or len(end_stop) == 0:
-                print("start_stop length:",len(start_stop),"end_stop length",len(end_stop))
+                #print("start_stop length:",len(start_stop),"end_stop length",len(end_stop))
                 return False
         if len(start_stop) > 1 or len(end_stop) > 1:
                 resolved = False
@@ -227,7 +228,7 @@ def inputValidator(start_stop, end_stop):
                         if resolved:
                                 break
                         for e in end_stop:
-                                print(s, '\n', e)
+                                #print(s, '\n', e)
                                 if s.sequence < e.sequence and s.route_direction == e.route_direction:
                                         start_stop, end_stop = s, e
                                         resolved = True
@@ -251,7 +252,7 @@ def wait_time(route, stop_id):
         data = rt.call_api(url)
         json_parsed = rt.write_file(data)
         realtime = get_times(json_parsed, route)
-        print ('Realtime:',realtime)
+        #print ('Realtime:',realtime)
                 
         if (realtime == "Due"):
             wait = 'Due Now'
@@ -275,10 +276,12 @@ def predict_request(request):
                         return HttpResponse('<p>Unfortunately, our data does not allow to us to make predictions for Route ' + route + '</p>')
                 elif route in routes_no_longer_in_service:
                         return HttpResponse('<p>Route ' + route + ' is no longer in service</p>')
+                elif route in routes_unsupported_by_data:
+                        return HttpResponse('<p>Route ' + route + ' not sufficiently supported by dataset, prediction unavailable</p>')
                 elif route not in routes_implemented:
                         return HttpResponse('<p>Route ' + route + ' not recognised</p>')
                 prediction = predictions_model(start_stop, end_stop, route, int(year), int(month), int(day), int(hour))
-                print("Predicted wait time is", prediction)
+                #print("Predicted wait time is", prediction)
                 wait = wait_time(route, start_stop)
                 return HttpResponse('<p>Wait Time: ' + wait + '</p><p>Travel Time: ' + prediction + '</p>')
 
@@ -338,7 +341,7 @@ def popStops(request):
                 start_stop, end_stop, route = str(g['start_stop']), str(g['end_stop']), g['route']
                 response = getStops(route, start_stop, end_stop)
 
-        print(response)
+        #print(response)
 
         context = {}
         context['stops'] = []
@@ -368,8 +371,8 @@ def predict_address(request):
         try:
             for key, i in latlng.items():
                 if key in "012345689":
-                   print(key)
-                   print(i)
+                   #print(key)
+                   #print(i)
                    lat1, lng1, lat2, lng2 = i[0], i[1], i[2], i[3]
                                 
                    stop1 = DbusStopsv3.objects.raw(query % (float(lat1), float(lat1), float(lng1), float(lng1), float(lat1)))[0].stop_id
@@ -407,7 +410,7 @@ def predict_address(request):
                                    final_route = result["route"]     
        
                    prediction = predictions_model(str(stop1), str(stop2), str(final_route), int(year), int(month), int(day), int(hour))
-                   print("prediction",prediction)
+                   #print("prediction",prediction)
                    prediction = prediction + walk_time
                
                    stops = getStops(str(final_route), str(stop1), str(stop2))
@@ -419,7 +422,7 @@ def predict_address(request):
            context["error"] =  "1"
            print(e)
 
-        print(context) 
+        #print(context) 
 
         return JsonResponse(context)
 
